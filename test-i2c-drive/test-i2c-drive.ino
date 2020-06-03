1201e16cab46d29d4b4f112c81131a13a01c4d40
openbsod <ispravnikov.spb@gmail.com>
2020-06-03 22:55:02 +0300

    add vertical-horizontal servo movements

--- test-i2c-drive/test-i2c-drive.ino
+++ test-i2c-drive/test-i2c-drive.ino
@@ -10,9 +10,12 @@ int Right_motor_go = 6;                        // (IN3)
 int Right_motor_back = 7;                      // (IN4)
 int Right_motor_en = 5;                        // (EN2)
 
-Servo myservo;
-int servo = 4;                                 // servo pin 4
+Servo myservo;                                 // steering servo pin 4
 int angle = 90;
+Servo hservo;                                  // horizontal servo pin 11 
+int hangle = 80;
+Servo vservo;                                  // vertical servo pin 12 
+int vangle = 70;
  
 //Pin Setup
 const int TrigPinR = 34;                       // left ultrasonic trig 
@@ -46,7 +49,12 @@ void setup()
   pinMode(EchoPinR, INPUT);
 
   myservo.attach(4);
-    
+  myservo.write(90);
+  hservo.attach(11);
+  hservo.write(80);
+  vservo.attach(12);
+  vservo.write(70);
+      
   pinMode(Left_motor_go,    OUTPUT);           // PIN 5 (PWM)
   pinMode(Left_motor_back,  OUTPUT);           // PIN 6 (PWM)
   pinMode(Right_motor_go,   OUTPUT);           // PIN 2 (PWM)
@@ -74,14 +82,19 @@ number = Wire.read();
 //Serial.print("data received: ");
 //Serial.println(number);
 
-if (number == 1) {
+  if (number == 1)
   run();
-}
-else{
-  if (number == 2)
+  else if (number == 2)
     stop();
-}
-}
+  else if (number == 3)
+    hleft();    
+  else if (number == 4)
+    vup();    
+  else if (number == 5)
+    vdown();    
+  else if (number == 6)
+    hright();
+  }
 }
 
 /*
@@ -90,6 +103,32 @@ void sendData(){
 Wire.write(number);
 }
 */
+
+// horizontal left servo
+void hleft()
+{
+  hangle = (hangle + 1) * 1;
+  hservo.write(hangle);
+}
+
+void vup()
+{
+  vangle = ( vangle + 1) * 1;
+  vservo.write(vangle);
+}
+
+// horizontal right servo
+void hright()
+{
+  hangle = ( hangle - 1) * 1;
+  hservo.write(hangle);
+}
+
+void vdown()
+{
+  vangle = ( vangle - 1) * 1;
+  vservo.write(vangle);
+}
 
 //the function to make the left ultrasonic controller calculate the distance
 void LeftSensor()
@@ -124,13 +163,13 @@ void stop()
 
 void back()
 {
-    //run forward
-    //left motor go
+  //run forward
+  //left motor go
   digitalWrite(Left_motor_go, HIGH);   //enable left motor go
   digitalWrite(Left_motor_back, LOW);  //prohibit left motro back
   analogWrite(Left_motor_en, 250);
 
-   //right motor go
+  //right motor go
   digitalWrite(Right_motor_go, HIGH);  //enable right motor go
   digitalWrite(Right_motor_back, LOW); //prohibit right motor back
   analogWrite(Right_motor_en, 250);
@@ -140,8 +179,8 @@ void back()
 
 void left()
 {
- //left motor stop
-   digitalWrite(Left_motor_go, LOW);    //prohibit left motor go
+  //left motor stop
+  digitalWrite(Left_motor_go, LOW);    //prohibit left motor go
   digitalWrite(Left_motor_back, LOW);  //prohibit left motor back
   analogWrite(Left_motor_en, 0);
 
@@ -185,7 +224,7 @@ void spin_left()
 
 void spin_right()
 {
- //left motor go
+  //left motor go
   digitalWrite(Left_motor_go, HIGH);    //enable left motor go
   digitalWrite(Left_motor_back, LOW);   //prohibit left motor back
   analogWrite(Left_motor_en, 230);
